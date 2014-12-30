185e0dc1a42992cfeab79f09f038b2a7cafa06bc
Adam Sealey <asealey@gmail.com>
2014-12-30 11:31:17 -0800

    Add volume specification to crits_mongo container in startDockerContainers.sh
    Allows persistent data storage on the host.  Also Add status messages

--- startDockerContainers.sh
+++ startDockerContainers.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Start the crits_mongo server
-docker rm crits_mongo && docker run -d --name crits_mongo -t crits_mongo
+echo "Starting crits_mongo"
+docker rm crits_mongo && docker run -d --name crits_mongo -v /data/db:/data/db -t crits_mongo
 
 # Start the crits_web server
+echo "Starting crits_web"
 docker rm crits_web && docker run -d -P --name crits_web --link crits_mongo:crits_mongo -t crits_web
+
+echo "Done"
