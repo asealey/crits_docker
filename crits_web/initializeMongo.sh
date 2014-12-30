6449232e1820849c0600cb26473ca9cb2d33a144
Adam Sealey <asealey@gmail.com>
2014-12-30 11:03:29 -0800

    Add mongo initialization script
    The crits_mongo container needs to be initialized from the crits_web
    instance.  This is a one-time operation.  Placing this script into the
    container to aid creation.  Plan to use this as a one-time operation for
    a container run.

---
+++ crits_web/initializeMongo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+user=''
+email=''
+
+usage () {
+  echo "Usage:
+  -u: User to create (Required)
+  -e: Email to specify for user (Required)"
+}
+
+while getopts ":h?u:e:" opt; do
+  case "$opt" in
+    h|\?)
+    usage
+    exit 0
+    ;;
+    u)  user=$OPTARG;;
+    e)  email=$OPTARG
+  esac
+done
+
+shift $((OPTIND-1))
+
+#[ "$1" = "--" ] && shift
+
+if [ -z "$user" ] || [ -z "$email" ]
+  then
+  usage
+  >&2 echo "Both User (-u) and Email (-e) are required"
+  exit 1
+fi
+
+echo "Creating user=$user, email='$email'"
+
+# Create collection in mongo
+#cd /data/crits
+#python manage.py create_default_collections
