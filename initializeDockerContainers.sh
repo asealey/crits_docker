9a2cf5596361bb8eedb91799c50280cacf8c5713
Adam Sealey <asealey@gmail.com>
2015-01-05 23:12:33 -0800

    Define fig config for automatic provisioning of docker containers.  Resolves #2
    Fig (Soon to be orchestrator) provides easy specification for how to spin
    up docker containers.  More robust than previous startup scripts.
    
    Usage:
    * fig -p crits build
    * fig -p crits start
    * fig -p crits ps
    
    Also update initialization script to leverage fig naming convention.

--- initializeDockerContainers.sh
+++ initializeDockerContainers.sh
@@ -2,5 +2,6 @@
 
 echo "Spinning initializer container for $user/$email"
 sleep 5 # Required to prevent spinning from happening before mongo is ready.  Not sure why the mongo container doesn't validate that
-docker run --link crits_mongo:crits_mongo -t crits_web /data/crits/initializeMongo.sh -u $user -e $email
+docker run --entrypoint=/bin/bash --link crits_mongo_1:crits_mongo -t crits_web /data/crits/initializeMongo.sh $@
+#docker run --link crits_mongo:crits_mongo -t crits_web /data/crits/initializeMongo.sh -u $user -e $email
 echo "Done initializing"
